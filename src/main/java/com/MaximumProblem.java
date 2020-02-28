7c19ad68003e57ea61facd8d20ac291273ff7f26
mangesh <mlatekar@gmail.com>
2020-02-28 16:32:40 +0530

    ADDED:Check Maximum Integer in given 3 Integers

--- src/main/java/com/MaximumProblem.java
+++ src/main/java/com/MaximumProblem.java
@@ -1,7 +1,19 @@
 package com;
 
 public class MaximumProblem {
+
     public static void main(String []args) {
         System.out.println("Welcome to Test Maximum Problem");
+    }
+
+    public Integer MaximumProblem(Integer a, Integer b, Integer c) {
+        Integer max=a;
+        if (b.compareTo(max)>0){
+            max=b;
+        }
+        if (c.compareTo(max)> 0){
+            max=c;
+        }
+        return max;
     }
 }
--- src/test/java/com/Test/TestMaximumProblem.java
+++ src/test/java/com/Test/TestMaximumProblem.java
@@ -1,4 +1,28 @@
 package com.Test;
 
+import com.MaximumProblem;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestMaximumProblem {
+    
+    MaximumProblem maximumProblem=new MaximumProblem();
+
+    @Test
+    public void inGivenThree_Integers_FirstInteger_IsMaximum() {
+        int maximum=maximumProblem.MaximumProblem(22,18,12);
+        Assert.assertEquals(22,maximum);
+    }
+
+    @Test
+    public void inGivenThree_Integers_SecondInteger_IsMaximum() {
+        int maximum=maximumProblem.MaximumProblem(10,30,25);
+        Assert.assertEquals(30,maximum);
+    }
+
+    @Test
+    public void inGivenThree_Integers_ThirdInteger_IsMaximum() {
+        int maximum=maximumProblem.MaximumProblem(20,40,55);
+        Assert.assertEquals(55,maximum);
+    }
 }
